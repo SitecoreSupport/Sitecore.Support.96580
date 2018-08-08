26732f1d06288959e41bc5a58f2e56ffe5c3321c
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2018-08-08 11:47:10 -0400

    Patch init

---
+++ src/Sitecore.Support.96580/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.96580")]
+[assembly: AssemblyProduct("Sitecore.Support.96580")]
+[assembly: ComVisible(false)]
